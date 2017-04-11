e59d5b63c59b7d5cde8a9810f6ecad8fc00502fd
Qomarullah <qomarullah@telkomsel.co.id>
2017-04-11 08:27:42 +0700

    Bismillah initial commit

---
+++ app/src/main/java/id/or/mtt/program/MainActivity.java
@@ -0,0 +1,18 @@
+package id.or.mtt.program;
+
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+
+        getSupportActionBar().setDisplayOptions(ActionBar.DISPLAY_SHOW_CUSTOM);
+        getSupportActionBar().setCustomView(R.layout.abs_layout);
+
+        setContentView(R.layout.activity_main);
+    }
+}
